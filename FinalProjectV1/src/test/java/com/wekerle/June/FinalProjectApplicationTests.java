a26bb2c5ca2a54255740b5428003e7f686d4ac4a
mmabbasi1374 <96060193+mmabbasi1374@users.noreply.github.com>
2022-06-20 22:26:55 +0430

    Create spring boot application
    with Java Maven ,I created a spring boot and packages(Model,Repository,Service,Controller and Exception).

---
+++ FinalProjectV1/src/test/java/com/wekerle/June/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wekerle.June;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
