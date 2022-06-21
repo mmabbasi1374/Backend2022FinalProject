18cdbc02b639666b6df6e2b74510a268f2ce175e
mmabbasi1374 <96060193+mmabbasi1374@users.noreply.github.com>
2022-06-21 13:53:44 +0430

    Create packages
    create packages (Model,Controller,Repository,Service and Exception) and add dependency in pom.xml and connect to data base in application.properties

---
+++ final2022JuneV2/src/main/java/com/wekerle/June/Final2022ProjectApplication.java
@@ -0,0 +1,15 @@
+package com.wekerle.June;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class Final2022ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Final2022ProjectApplication.class, args);
+	}
+
+}
---
+++ final2022JuneV2/src/test/java/com/wekerle/June/Final2022ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wekerle.June;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final2022ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
