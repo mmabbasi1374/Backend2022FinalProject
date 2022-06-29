14e87f41d28d2559abab52fe5da3bae712d53f74
mmabbasi1374 <96060193+mmabbasi1374@users.noreply.github.com>
2022-06-29 19:18:37 +0430

    SQL procedures
    wSQL procedure and some fake data add to SQL fil

--- final2022JuneV2/src/main/java/com/wekerle/June/Service/MovieService.java
+++ final2022JuneV2/src/main/java/com/wekerle/June/Service/MovieService.java
@@ -4,9 +4,14 @@
  */
 package com.wekerle.June.Service;
 
+import com.wekerle.June.Model.Movie;
 import com.wekerle.June.Repository.MovieRepository;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.ParameterMode;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +22,33 @@ public class MovieService {
 
     public MovieService(MovieRepository repo) {
         this.repo = repo;
+    }
+    
+    public Boolean addNewMovie(String title, Integer length, Integer age, String director){
+        try{
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("addNewMovie");
+            EntityManager em = emf.createEntityManager();
+            StoredProcedureQuery spq = em.createStoredProcedureQuery("addNewMovie");
+
+            spq.registerStoredProcedureParameter("titleIN", String.class, ParameterMode.IN);
+            spq.registerStoredProcedureParameter("lengthIN", Integer.class, ParameterMode.IN);
+            spq.registerStoredProcedureParameter("ageIN", Integer.class, ParameterMode.IN);
+            spq.registerStoredProcedureParameter("directorIN", String.class, ParameterMode.IN);
+
+            spq.setParameter("titleIN", title);
+            spq.setParameter("lengthIN", length);
+            spq.setParameter("ageIN", age);
+            spq.setParameter("directorIN", director);
+
+            spq.execute();
+            emf.close();
+            em.close();
+            
+            return true;
+        }
+        catch(Exception ex){
+            return false;
+        }
     }
 
     
--- final2022JuneV2/src/main/java/com/wekerle/June/Service/UserService.java
+++ final2022JuneV2/src/main/java/com/wekerle/June/Service/UserService.java
@@ -5,20 +5,61 @@
 
 package com.wekerle.June.Service;
 
+import com.wekerle.June.Exception.registerInvalidException;
+import com.wekerle.June.Exception.takenEmailException;
+import com.wekerle.June.Exception.takenPhoneException;
+import com.wekerle.June.Model.User;
 import com.wekerle.June.Repository.UserRepository;
 import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
 import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
     private final UserRepository repo;
     @PersistenceContext
     private EntityManager em;
-
+   
     public UserService(UserRepository repo) {
         this.repo = repo;
+        
     }
+    
+    public User addNewUser(User user){
+        return this.repo.save(user);
+        
+    }
+    
+    public String checkemail(String email){
+        StoredProcedureQuery spq = em.createStoredProcedureQuery("addNewUser");
+        
+        spq.registerStoredProcedureParameter("email", String.class,ParameterMode.IN);
+        
+        
+        spq.setParameter("email", email);
+        
+        spq.execute();
+        
+        return "youn wrote your email address" ;
+    }
+    
+    public String checkPhone(String phone){
+        StoredProcedureQuery spq = em.createStoredProcedureQuery("checkPhone");
+        
+        spq.registerStoredProcedureParameter("phone", String.class, ParameterMode.IN);
+        
+        spq.setParameter("phone", phone);
+        
+        spq.execute();
+        
+        return "ypu wrote your phone";
+    }
+    
+    
+    
 
     
 }
