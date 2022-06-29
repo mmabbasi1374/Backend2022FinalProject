ce9f7e0965f6c55db71d58420b3b63125e58a793
mmabbasi1374 <96060193+mmabbasi1374@users.noreply.github.com>
2022-06-29 13:29:34 +0430

    add all annotation
    add all annotation for every classes in every layer

--- final2022JuneV2/src/main/java/com/wekerle/June/Service/UserService.java
+++ final2022JuneV2/src/main/java/com/wekerle/June/Service/UserService.java
@@ -2,12 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+import org.springframework.stereotype.service;
 package com.wekerle.June.Service;
 
-/**
- *
- * @author parsarayaneh
- */
+@service
 public class UserService {
     
 }
